e382833b1e9d07b05c593e8d566bd12d860bf78e
Kim Sang Min <yhoo112@naver.com>
2020-09-08 10:02:16 +0900

    0908class

---
+++ JavaStreamProject/src/streams/StreamExample.java
@@ -0,0 +1,38 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class StreamExample {
+	public static void main(String[] args) {
+		//컬렉션 => 외부반복자
+		List<String> users = Arrays.asList("홍길동", "최재영", "민해주", "김상민");
+		System.out.println("외부반복자 사용...");
+		Iterator<String> iter = users.iterator(); //반복자(iterator)
+		while (iter.hasNext()) {
+			String str = iter.next();
+			System.out.println(str.toString());
+		}
+		
+		
+		//스트림 => 내부반복자
+		System.out.println("내부반복자 사용...");
+		Stream<String> stremas = users.stream();
+		stremas.forEach(t -> System.out.println(t.toString()));
+		
+		//람다식 이전
+//		stremas.forEach(new Consumer<String>() {
+//
+//			@Override
+//			public void accept(String t) {
+//				System.out.println(t.toString());
+//				
+//			}
+//			
+//		});
+		
+	}
+}
---
+++ JavaStreamProject/src/streams/StreamExample2.java
@@ -0,0 +1,10 @@
+package streams;
+class Student {
+	String name;
+	int score;
+}
+public class StreamExample2 {
+	public static void main(String[] args) {
+		
+	}
+}
